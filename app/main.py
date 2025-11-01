fde997b357c3b4c37c3ef5e3b81fba11a2841ada
aoncodev <aoncodev@gmail.com>
2025-11-01 18:28:06 +0900

    adding server config to main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import extract
 
-app = FastAPI(title="VIN Extractor API", version="1.0")
+app = FastAPI(title="VIN Extractor API", version="1.0",servers=[{"url": "http://43.200.233.218:8000", "description": "EC2 Server"}],)
 
 app.add_middleware(
     CORSMiddleware,
