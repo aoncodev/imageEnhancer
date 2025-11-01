9663c8fde790e68078742c5738edae7c53218332
aoncodev <aoncodev@gmail.com>
2025-11-01 18:33:22 +0900

    removing cors

--- app/main.py
+++ app/main.py
@@ -3,17 +3,7 @@ load_dotenv()
 
 
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 from app.routers import extract
 
-app = FastAPI(title="VIN Extractor API", version="1.0",servers=[{"url": "http://43.200.233.218:8000", "description": "EC2 Server"}],)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],  # for now allow all; can restrict later
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
+app = FastAPI(title="VIN Extractor API", version="1.0")
 app.include_router(extract.router, prefix="/api", tags=["VIN"])
